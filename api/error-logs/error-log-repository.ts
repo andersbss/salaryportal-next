f9060ee50ba4e843efc3ce0e487655638f00d1a5
Anders Strandseter <anders@lyonfuture.no>
2022-10-31 21:23:56 +0100

    feat: initial report data model

--- api/error-logs/error-log-repository.ts
+++ api/error-logs/error-log-repository.ts
@@ -1,7 +1,6 @@
 import ErrorLogModelSchema, { ErrorLogModel } from './error-log-model';
 
 const create = async (input: Omit<ErrorLogModel, 'id'>): Promise<ErrorLogModel> => {
-  console.log('hwello');
   return ErrorLogModelSchema.create<ErrorLogModel>(input);
 };
 
--- api/reports/report-model.ts
+++ api/reports/report-model.ts
@@ -1,12 +1,135 @@
 import { Schema as MongooseSchema, model, models } from 'mongoose';
 
 export interface ReportModel {
+  // Required
   id: string;
-  title: string;
+  jobTitle: string;
+  age: number;
+  field: string;
+  sector: 'private' | 'public';
+  totalYearlySalary: number;
+  county: string;
+  workPlace: 'remote' | 'office' | 'hybrid';
+  degrees: string[];
+  tags: string[];
+  totalYearsExperienceInField: number;
+  yearsInCurrentRole: number;
+  workTimePercent: number;
+  vacationDays: number;
+  workFlow: 'fulltime' | 'parttime' | 'freelance' | 'substitute' | 'call guard' | 'other';
+
+  // Optional
+  leaderShipPosition: boolean | null;
+  graduateYear: number | null;
+  graduateSchool: string | null;
+  city: string | null;
+  flextime: boolean | null;
+  seniority: string | null;
+  hourlyWage: string | null;
+  overtimePercentage: number | null;
+  benefits: string[] | null;
+  otherCompensation: string[] | null;
+  otherComments: string | null;
 }
 
 const Schema = new MongooseSchema<ReportModel>({
-  title: String,
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  field: {
+    type: String,
+    required: true,
+  },
+  sector: {
+    type: String,
+    required: true,
+  },
+  totalYearlySalary: {
+    type: Number,
+    required: true,
+  },
+  county: {
+    type: String,
+    required: true,
+  },
+  workPlace: {
+    type: String,
+    required: true,
+  },
+  degrees: {
+    type: [String],
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  totalYearsExperienceInField: {
+    type: Number,
+    required: true,
+  },
+  yearsInCurrentRole: {
+    type: Number,
+    required: true,
+  },
+  workTimePercent: {
+    type: Number,
+    required: true,
+  },
+  vacationDays: {
+    type: Number,
+    required: true,
+  },
+  workFlow: {
+    type: String,
+    required: true,
+  },
+
+  leaderShipPosition: {
+    type: Boolean,
+    required: false,
+  },
+  graduateYear: {
+    type: Number,
+    required: false,
+  },
+  graduateSchool: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: false,
+  },
+  flextime: {
+    type: Boolean,
+    required: false,
+  },
+  seniority: {
+    type: String,
+    required: false,
+  },
+  hourlyWage: {
+    type: String,
+    required: false,
+  },
+  overtimePercentage: {
+    type: Number,
+    required: false,
+  },
+  benefits: {
+    type: [String],
+    required: false,
+  },
+  otherCompensation: {
+    type: [String],
+    required: false,
+  },
+  otherComments: {
+    type: String,
+    required: false,
+  },
 });
 
 const ReportModelSchema = models.Report || model<ReportModel>('Report', Schema);
