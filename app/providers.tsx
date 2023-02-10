17985b7d0f35cf0c57b56683fbd2b4aa359f0444
Anders Strandseter <anders@lyonfuture.no>
2023-02-10 20:35:21 +0100

    feat: query client

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,14 +1,30 @@
 'use client';
 
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
 
 type GlobalProviderProps = {
   children: React.ReactNode;
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      retry: false,
+    },
+    queries: {
+      retry: false,
+    },
+  },
+});
+
 const GlobalProvider = ({ children }: GlobalProviderProps) => {
   // This is where we can wrap our app with global providers
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
+  );
 };
 
 export default GlobalProvider;
