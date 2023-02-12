1f01f2556a482f5a8083083554cb4ea6c669b6a8
Anders Strandseter <anders@lyonfuture.no>
2023-02-12 23:23:39 +0100

    feat: todo

--- client/modules/report/forms/new-report/personal-info-form-input.ts
+++ client/modules/report/forms/new-report/personal-info-form-input.ts
@@ -1,5 +1,6 @@
 import { RouterOutput } from '@client/trpc';
 
+//TOOD: Find better enum/literal type solution
 export type Gender = keyof RouterOutput['enums']['gender'];
 
 export type PersonalInfoFormInput = {
