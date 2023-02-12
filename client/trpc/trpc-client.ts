29137345324ccf8f7c60d0093d9d77336f1d224a
Anders Strandseter <anders@lyonfuture.no>
2023-02-12 21:34:12 +0100

    refactor: new report page to pages dir

--- client/trpc/trpc-client.ts
+++ client/trpc/trpc-client.ts
@@ -3,7 +3,7 @@ import { createTRPCNext } from '@trpc/next';
 
 import type { AppRouter } from '@server/trpc/trpc-router';
 
-function getBaseUrl() {
+const getBaseUrl = () => {
   if (typeof window !== 'undefined')
     // browser should use relative path
     return '';
@@ -15,37 +15,62 @@ function getBaseUrl() {
     return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
   // assume localhost
   return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+};
 
 export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
-    return {
-      links: [
-        httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/ssr
-           **/
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
-      /**
-       * @link https://tanstack.com/query/v4/docs/reference/QueryClient
-       **/
-      queryClientConfig: {
-        defaultOptions: {
-          mutations: {
-            retry: false,
-          },
-          queries: {
-            retry: false,
+    if (typeof window !== 'undefined') {
+      // during client requests
+      return {
+        //transformer: superjson, // optional - adds superjson serialization
+        links: [
+          httpBatchLink({
+            url: '/api/trpc',
+          }),
+        ],
+        queryClientConfig: {
+          defaultOptions: {
+            mutations: {
+              retry: false,
+            },
+            queries: {
+              retry: false,
+            },
           },
         },
-      },
+      };
+    }
+    return {
+      //transformer: superjson, // optional - adds superjson serialization
+      links: [
+        httpBatchLink({
+          // The server needs to know your app's full url
+          url: `${getBaseUrl()}/api/trpc`,
+          /**
+           * Set custom request headers on every request from tRPC
+           * @link https://trpc.io/docs/v10/header
+           */
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                connection: _connection,
+                ...headers
+              } = ctx.req.headers;
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                'x-ssr': '1',
+              };
+            }
+            return {};
+          },
+        }),
+      ],
     };
   },
-  /**
-   * @link https://trpc.io/docs/ssr
-   **/
-  ssr: false,
+  ssr: true,
 });
