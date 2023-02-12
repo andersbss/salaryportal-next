aeb8cc5c1cc848ec7a068186217e9ceac9412431
Anders Strandseter <anders@lyonfuture.no>
2023-02-12 20:31:33 +0100

    fix: remove unused lib

--- api-modules/utils/error/401.ts
+++
@@ -1,8 +0,0 @@
-export class UnauthorizedError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'UnauthorizedError';
-  }
-}
-
-export default UnauthorizedError;
--- api-modules/utils/error/500.ts
+++
@@ -1,9 +0,0 @@
-// With a message and a name
-export class InternalServerError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'InternalServerError';
-  }
-}
-
-export default InternalServerError;
