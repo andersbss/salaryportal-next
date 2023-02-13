ad222371bd847f6ed18ebc390a9a8430545027a6
Anders Strandseter <anders@lyonfuture.no>
2023-02-13 20:09:21 +0100

    feat: initial wizard progress component

---
+++ client/ui/wizard/wizard progress/index.ts
@@ -0,0 +1,2 @@
+export * from './wizard-progress';
+export { default as WizardProgress } from './wizard-progress';
---
+++ client/ui/wizard/wizard progress/wizard-progress.tsx
@@ -0,0 +1,9 @@
+const WizardProgress = () => {
+  return (
+    <div>
+      <div>Wizard Progress</div>
+    </div>
+  );
+};
+
+export default WizardProgress;
