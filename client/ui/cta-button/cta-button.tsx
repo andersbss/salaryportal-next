859cb2bf1d712e038ddb7adbc1541a675a364ae6
Anders Strandseter <anders@lyonfuture.no>
2023-02-19 18:34:11 +0100

    refactor: duplicate cta button styling

--- client/ui/cta-button/cta-button.tsx
+++ client/ui/cta-button/cta-button.tsx
@@ -10,22 +10,19 @@ export type CtaButtonProps = {
 };
 
 const CtaButton = ({ children, action = () => {} }: CtaButtonProps): JSX.Element => {
+  const className =
+    'rounded-full border border-transparent bg-green-500 p-4 text-xl font-semibold text-white shadow-md transition duration-100 ease-in hover:border-green-500 hover:bg-transparent hover:text-slate-900 dark:text-white hover:dark:text-white ';
+
   if (typeof action === 'string') {
     return (
-      <Link
-        href={action}
-        className="rounded-full border border-transparent bg-green-500 p-4 text-xl font-semibold text-white shadow-md transition duration-100 ease-in hover:border-green-500 hover:bg-transparent hover:text-slate-900 dark:text-white hover:dark:text-white"
-      >
+      <Link href={action} className={className}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button
-      onClick={action}
-      className="rounded-full border border-transparent bg-green-500 p-4 text-xl font-semibold text-white shadow-md transition duration-100 ease-in hover:border-green-500 hover:bg-transparent hover:text-slate-900 dark:text-white hover:dark:text-white"
-    >
+    <button onClick={action} className={className}>
       {children}
     </button>
   );
